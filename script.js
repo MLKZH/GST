af464bb0a2b3c32904852eb046e2f359b2740d0c
MLKZH <gagagugu980@gmail.com>
2024-11-15 08:15:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+﻿document.querySelector('.chat-widget button').addEventListener('click', function() {
+    alert('Чат с поддержкой еще не доступен. Пожалуйста, свяжитесь с нами через форму на сайте.');
+});
